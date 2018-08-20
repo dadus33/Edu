7b9a6341fd56757eb8a308b62da5d2b259d26a35
dadus33 <longlivevladerius@gmail.com>
2018-08-20 16:06:41 +0300

    Add project files.

---
+++ Edu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Edu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
